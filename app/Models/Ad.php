ab91f775e54f378e5ade74f6ee11c8b75ee3994c
Ortneil <ortneil@gmail.com>
2020-12-30 23:37:45 +0200

    Defined functions of the model

--- app/Models/Ad.php
+++ app/Models/Ad.php
@@ -19,5 +19,16 @@ class Ad extends Model
         'currency_id'
     ];
 
+    public function get_category(){
+        return $this->belongsTo("App\Models\Category", 'category_id', 'category_id');
+    }
+
+    public function get_images(){
+        return $this->hasMany("App\Models\Image", 'ad_id', 'ad_id');
+    }
+
+    public function get_currency(){
+        return $this->belongsTo("App\Models\Currency", 'currency_id', 'currency_id');
+    }
 
 }
